083dc124129e12b13a9ba68d50b08e265dc84da3
Hrushikesh Jadhav <rushi611@gmail.com>
2022-07-10 14:42:47 +0530

    map

--- falafel-driver/src/screens/OrdersScreen.js
+++ falafel-driver/src/screens/OrdersScreen.js
@@ -1,15 +1,44 @@
-import { useRef, useMemo } from "react";
-import { View, Text, FlatList } from "react-native";
 import BottomSheet from "@gorhom/bottom-sheet";
-import orders from "../../assets/data/orders.json";
+import { useMemo, useRef } from "react";
+import { FlatList, Text, useWindowDimensions, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import MapView, { Marker } from "react-native-maps";
+import orders from "../../assets/data/orders.json";
 import OrderItem from "../components/OrderItem";
+import { Entypo } from "@expo/vector-icons";
 
 const OrdersScreen = () => {
   const bottomSheetRef = useRef(null);
+  const { width, height } = useWindowDimensions();
   const snapPoints = useMemo(() => ["12%", "95%"], []);
   return (
     <GestureHandlerRootView style={{ backgroundColor: "lightblue", flex: 1 }}>
+      <MapView
+        style={{
+          height,
+          width,
+        }}
+        showsUserLocation
+        followsUserLocation
+      >
+        {orders.map((order) => (
+          <Marker
+            key={order.id}
+            title={order.Restaurant.name}
+            description={order.Restaurant.address}
+            coordinate={{
+              latitude: order.Restaurant.lat,
+              longitude: order.Restaurant.lng,
+            }}
+          >
+            <View
+              style={{ backgroundColor: "green", padding: 5, borderRadius: 20 }}
+            >
+              <Entypo name="shop" size={24} color="white" />
+            </View>
+          </Marker>
+        ))}
+      </MapView>
       <BottomSheet
         ref={bottomSheetRef}
         snapPoints={snapPoints}
